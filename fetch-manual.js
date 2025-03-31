969db98e75bdac3fbc29e6b01fe909d55d6df951
Torsten Werner <torsten.werner@westernacher.com>
2025-03-31 20:13:57 +0300

    refactor the fetch-*.js scripts

--- fetch-manual.js
+++ fetch-manual.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import {fetchText, fetchUrls} from "./fetch-util.js";
+import {fetchUrls, processSite} from "./fetch-util.js";
 
 /**
  * Fetches the URLs of all target pages.
@@ -9,43 +8,4 @@ function fetchAllUrls() {
     return fetchUrls('https://handbuch.bea-brak.de/', '.tab-content a');
 }
 
-let i = 0;
-
-/**
- * Fetches the main text from the url and saves it to a file.
- * @param {string} url
- */
-async function fetchAndSaveText(url) {
-    const prefix = String(i++).padStart(3, '0');
-    const topic = url.replace(/.*\//, '').substring(0, 16);
-    const path = `manual/${prefix}-${topic}.txt`;
-    const text = await fetchText(url, '.col-md-9');
-    fs.writeFileSync(path, text);
-    fs.appendFileSync("webapp/dist/manual-full.txt", text);
-}
-
-function prepare() {
-    const directory = "manual";
-    if (!fs.existsSync(directory)) {
-        fs.mkdirSync(directory);
-    }
-    const files = fs.readdirSync(directory);
-    for (const file of files) {
-        if (file.endsWith('.txt')) {
-            const filePath = `${directory}/${file}`;
-            fs.unlinkSync(filePath);
-            // console.log(`Deleted: ${filePath}`);
-        }
-    }
-    if (!fs.existsSync("webapp/dist")) {
-        fs.mkdirSync("webapp/dist");
-    }
-    fs.writeFileSync("webapp/dist/manual-full.txt", "");
-}
-
-prepare();
-
-const targetUrls = await fetchAllUrls();
-console.log(`fetching ${targetUrls.length} pages`);
-
-await Promise.all(targetUrls.map(async url => await fetchAndSaveText(url)));
+await processSite('manual', fetchAllUrls, '.col-md-9');
--- fetch-portal.js
+++ fetch-portal.js
@@ -1,13 +1,12 @@
-import fs from 'fs';
-import {fetchText, fetchUrls} from "./fetch-util.js";
+import {fetchUrls, processSite} from "./fetch-util.js";
 
 /**
  * Fetches all page URLs from the main page
- * @param {string} url of the main pages
  * @returns {Promise<string[]>} the URLs of the child pages
  */
-function fetchMainUrls(url) {
-    return fetchUrls(url, 'nav[aria-label="Fragen und Antworten"] a', 'https://portal.beasupport.de');
+function fetchMainUrls() {
+    return fetchUrls('https://portal.beasupport.de/fragen-antworten', 'nav[aria-label="Fragen und Antworten"] a',
+        'https://portal.beasupport.de');
 }
 
 /**
@@ -24,48 +23,9 @@ function fetchTargetUrls(url) {
  * @returns {Promise<string[]>}
  */
 async function fetchAllUrls() {
-    const pageUrls = await fetchMainUrls('https://portal.beasupport.de/fragen-antworten');
-    const allUrls = await Promise.all(pageUrls.map(async url => await fetchTargetUrls(url)));
+    const mainUrls = await fetchMainUrls();
+    const allUrls = await Promise.all(mainUrls.map(async url => await fetchTargetUrls(url)));
     return allUrls.flat();
 }
 
-let i = 0;
-
-/**
- * Fetches the main text from the url and saves it to a file.
- * @param {string} url
- */
-async function fetchAndSaveText(url) {
-    const prefix = String(i++).padStart(3, '0');
-    const topic = url.replace(/.*\//, '').substring(0, 16);
-    const path = `portal/${prefix}-${topic}.txt`;
-    const text = await fetchText(url, '.faq-details');
-    fs.writeFileSync(path, text);
-    fs.appendFileSync("webapp/dist/portal-full.txt", text);
-}
-
-function prepare() {
-    const directory = "portal";
-    if (!fs.existsSync(directory)) {
-        fs.mkdirSync(directory);
-    }
-    const files = fs.readdirSync(directory);
-    for (const file of files) {
-        if (file.endsWith('.txt')) {
-            const filePath = `${directory}/${file}`;
-            fs.unlinkSync(filePath);
-            // console.log(`Deleted: ${filePath}`);
-        }
-    }
-    if (!fs.existsSync("webapp/dist")) {
-        fs.mkdirSync("webapp/dist");
-    }
-    fs.writeFileSync("webapp/dist/portal-full.txt", "");
-}
-
-prepare();
-
-const targetUrls = await fetchAllUrls();
-console.log(`fetching ${targetUrls.length} pages`);
-
-await Promise.all(targetUrls.map(async url => await fetchAndSaveText(url)));
+await processSite('portal', fetchAllUrls, '.faq-details');
